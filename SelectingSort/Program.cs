ec7e3493ff1b944d0feff00354fde0aa64f6374e
Daniil Kantser <kancerdanya20001@gmail.com>
2023-01-28 20:26:35 +0300

    added simple example

--- SelectingSort/Program.cs
+++ SelectingSort/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿void SortSelection(int[] collection)
+
+{
+    int size = collection.Length;
+
+    for (int i = 0; i < size - 1; i++)
+    {
+       int pos = i;
+       for (int j = 0; j < size; j++)
+       {
+        if(collection[j] < collection[pos]) pos = j;
+       } 
+       int temp = collection[i];
+       collection[i] = collection[pos];
+       collection[pos] = temp;
+    }
+}
+
+int[] array = { 7, 6, 3, 4, 5, 1, 2, 3 };
+
+Console.WriteLine($"[{String.Join(',', array)}]");
+SortSelection(array);
+Console.WriteLine($"[{String.Join(',', array)}]");
